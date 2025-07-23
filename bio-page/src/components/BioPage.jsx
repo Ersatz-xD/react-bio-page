7d41dbae827600dbdb8c7a2b2b9b50099a894423
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-07-24 00:28:14 +0500

    started typing some shi

--- bio-page/src/components/BioPage.jsx
+++ bio-page/src/components/BioPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './BioPage.css'
 
 const BioPage = () => {
   return (
-    <div>BioPage</div>
+    <div className='main-box'>
+      <h1>Ayaan</h1>
+      <p>Welcome to my bio page!</p>
+    </div>
   )
 }
 
