3f49dcb01e9053b92dfd8f5a9985a9889265fdd7
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-07-24 21:42:05 +0500

    Update LinkedInBtn.jsx

--- bio-page/src/components/LinkedInBtn.jsx
+++ bio-page/src/components/LinkedInBtn.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./LinkedInBtn.css";
 
 const LinkedInBtn = () => {
   return (
-    <div>LinkedInBtn</div>
+    <div>
+    <button className='linkedIn-btn'
+      onClick={() => window.open("https://www.linkedin.com/in/ayaan-ahmed-khan-448600351")}>
+      Connect
+      
+    </button>
+    
+    </div>
   )
 }
 
